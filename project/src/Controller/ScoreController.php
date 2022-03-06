c5e0bcf1f2b05d2228c065bd18779cb7c6e05ade
Nasake <wilfrid.salles-otta@supinternet.fr>
2022-03-06 16:34:14 +0100

    Score ( not finished )

---
+++ project/src/Controller/ScoreController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ScoreController extends AbstractController
+{
+    #[Route('/score', name: 'score')]
+    public function index(): Response
+    {
+        setScore(this->getScore()+ 1);
+    }
+}
